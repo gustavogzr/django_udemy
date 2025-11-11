db71b173db43aea1d9988568a1fdfc51e96e0555
gustavogzr <gustavogzr@petrobras.com.br>
2025-11-10 22:39:33 -0300

    Aula 92 - Ajuste de ASGI para nova versão de Django

--- realtime/realtime/settings.py
+++ realtime/realtime/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configuração específica para o Channels
-ASGI_APPLICATION = 'realtime.routing.application'
+ASGI_APPLICATION = 'realtime.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
